c94e6c8bcf116c9b8629f368b38d1e60f3d247e4
so0sora <latt03200909@gmail.com>
2024-10-21 12:50:26 +0900

    Update fade_in_out_effect.py

--- Biorhythm/fade_in_out_effect.py
+++ Biorhythm/fade_in_out_effect.py
@@ -22,31 +22,21 @@ class FadeInOut(arcade.View):
         self.all_sprite_list.append(self.background)
 
     def on_draw(self):
-        # 모든 스프라이트를 그립니다
         self.all_sprite_list.draw()
     
     def on_update(self, delta_time):
         # 투명도 조절  
-        if self.whether_fade:  # 아웃: 어두워짐
+        if self.whether_fade == True:  # 아웃: 어두워짐
             self.alpha += self.fade_speed
             if self.alpha >= 255:
-                self.alpha = 255
-                # 다음 뷰로 전환 (완전히 어두워지면 FadeInOut 인스턴스 생성)
-                fade_in_view = FadeInOut(self.previous_view, self.next_view, False, self.fade_speed)  # 인스턴스 생성
-                self.window.show_view(fade_in_view)  # 인스턴스 전달
-                return
+                if self.alpha == 255:
+                   self.window.show_view(FadeInOut(self.previous_view, self.next_view, False, self.fade_speed))
+                   return
 
         else:  # 인: 밝아짐 
             self.alpha -= self.fade_speed
-            if self.alpha <= 0:
-                self.alpha = 0
-
-                # 추가 디버깅: self.next_view가 arcade.View 인스턴스인지 확인
-                if isinstance(self.next_view, arcade.View):
-                    # 다음 뷰로 전환 (완전히 밝아지면 새로운 화면을 보여줌)
-                    self.window.show_view(self.next_view)  # 이미 인스턴스이므로 바로 전달
-                else:
-                    print(f"Error: self.next_view is not an arcade.View instance. It is: {type(self.next_view)}")
+            if self.alpha == 0:
+                self.window.show_view(self.next_view)
                 return
                 
         # 알파값을 0과 255 사이로 제한
